d542590109f63b169d7e19280ca152132f58c36a
Emily Hosoya <emilyhosoya@gmail.com>
2014-11-07 10:20:11 -0700

    Use SimpleListFilter to show only current jobs and users by default

--- timecard/admin.py
+++ timecard/admin.py
@@ -1,20 +1,48 @@
+from django.utils.translation import ugettext_lazy as _
 from django.contrib import admin
 from adminsortable.admin import SortableAdminMixin
 from timecard.models import Job, User
 
+class IsActiveFilter(admin.SimpleListFilter):
+	title = _('Status')
+
+	parameter_name = 'active'
+
+	def lookups(self, request, model_admin):
+		return (
+			(None, _('Active')),
+			('no', _('Not Active')),
+			('all', _('All')),
+		)
+
+	def choices(self, cl):
+		for lookup, title in self.lookup_choices:
+			yield {
+				'selected': self.value() == lookup,
+				'query_string': cl.get_query_string({
+					self.parameter_name: lookup,
+				}, []),
+				'display': title,
+			}
+
+	def queryset(self, request, queryset):
+		if self.value() == None:
+			return queryset.filter(isActive=True)
+		elif self.value() == 'no':
+			return queryset.filter(isActive=False)
 
 # Register your models here.
 class JobAdmin(SortableAdminMixin, admin.ModelAdmin):
-    fields = ['job_active', 'job_name']
+    fields = ['isActive', 'job_name']
     list_display = ('job_name', 'isActive')
-    list_filter = ['isActive']
+    list_filter = (IsActiveFilter,)
     search_fields = ['job_name']
 admin.site.register(Job, JobAdmin)
 
 
 class UserAdmin(admin.ModelAdmin):
-    fields = ['user_active', 'first_name', 'last_name']
+    fields = ['isActive', 'first_name', 'last_name']
     list_display = ('first_name', 'last_name', 'isActive')
-    list_filter = ['isActive']
+    list_filter = (IsActiveFilter,)
     search_fields = ['first_name', 'last_name']
 admin.site.register(User, UserAdmin)
